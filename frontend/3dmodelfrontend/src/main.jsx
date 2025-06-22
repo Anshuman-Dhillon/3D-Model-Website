c98ac60a78e7f3c175298c9bf08f05a81fed2376
ABhatti167 <armaan.bhatti@mail.utoronto.ca>
2025-06-21 23:36:08 -0400

    l

--- frontend/3dmodelfrontend/src/main.jsx
+++ frontend/3dmodelfrontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../component design/index.css'
+import './component design/index.css'
 import App from './pages/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
