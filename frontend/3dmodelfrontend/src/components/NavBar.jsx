503b7aa5f91d3a1be9a083d5448e906f2587039a
ABhatti167 <armaan.bhatti@mail.utoronto.ca>
2025-06-23 14:48:01 -0400

    nav

--- frontend/3dmodelfrontend/src/components/NavBar.jsx
+++ frontend/3dmodelfrontend/src/components/NavBar.jsx
@@ -1,17 +1,87 @@
 import React from 'react';
 
 function NavBar() {
-    return (
-        <>
-            <nav className="navbar navbar-light bg-light justify-content-between px-3">
-            <a className="navbar-brand">Navbar</a>
-            <form className="form-inline d-flex">
-                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary px-3" style={{ height: '75px', fontSize: '1.1rem' }}>
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"         // Bootstrap 5 uses data-bs-toggle, not data-toggle
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="visually-hidden">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">
+                Disabled
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-light" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default NavBar;
