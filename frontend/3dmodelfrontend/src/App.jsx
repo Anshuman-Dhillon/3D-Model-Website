e6b0b37796514a6109bfe39aadbdb85e9df7842a
ABhatti167 <armaan.bhatti@mail.utoronto.ca>
2025-06-24 01:45:12 -0400

    d

--- frontend/3dmodelfrontend/src/App.jsx
+++ frontend/3dmodelfrontend/src/App.jsx
@@ -3,7 +3,7 @@ import './pages design/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';//used to render whatever is in router in main.jsx
 
 function App() {
   return (
